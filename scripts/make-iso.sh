3ef857354315bee7d604360ad19a65f29226b9e9
Evan Allen <evanallen13@github.com>
2025-12-20 13:24:05 -0500

    Refactor ISO creation script with updated directory names

--- scripts/make-iso.sh
+++ scripts/make-iso.sh
@@ -1,32 +1,37 @@
+
 #!/usr/bin/env bash
 set -e
 
 ISO_NAME=$1
-WORKDIR=iso
 ROOTFS=rootfs
+ISO_DIR=iso
 
-sudo rm -rf $WORKDIR
-mkdir -p $WORKDIR/{boot/grub,live}
+sudo rm -rf "$ISO_DIR"
+mkdir -p "$ISO_DIR"/{boot/grub,live}
 
-# Squash rootfs
-sudo mksquashfs $ROOTFS $WORKDIR/live/filesystem.squashfs -comp xz -e boot
+# Create SquashFS
+sudo mksquashfs \
+  "$ROOTFS" \
+  "$ISO_DIR/live/filesystem.squashfs" \
+  -comp xz \
+  -e boot
 
-# Minimal GRUB config
-cat <<EOF | sudo tee $WORKDIR/boot/grub/grub.cfg
+# Copy kernel + initrd
+sudo cp "$ROOTFS"/boot/vmlinuz-* "$ISO_DIR/vmlinuz"
+sudo cp "$ROOTFS"/boot/initrd.img-* "$ISO_DIR/initrd.img"
+
+# GRUB config
+cat <<EOF | sudo tee "$ISO_DIR/boot/grub/grub.cfg"
 set default=0
 set timeout=3
 
-menuentry "Minimal Debian" {
+menuentry "Minimal Debian Live" {
     linux /vmlinuz boot=live quiet
     initrd /initrd.img
 }
 EOF
 
-# Copy kernel + initrd from rootfs
-sudo cp $ROOTFS/boot/vmlinuz-* $WORKDIR/vmlinuz
-sudo cp $ROOTFS/boot/initrd.img-* $WORKDIR/initrd.img
-
 # Create hybrid ISO (BIOS + UEFI)
 sudo grub-mkrescue \
   -o "$ISO_NAME" \
-  "$WORKDIR"
+  "$ISO_DIR"
